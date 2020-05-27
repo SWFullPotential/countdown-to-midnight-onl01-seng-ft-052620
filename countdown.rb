31822e436f3ee6d3d0575b09c203b2e756eb89e3
Samantha Wabi <swfullpotential@gmail.com>
2020-05-27 20:25:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number >= 1
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while runtime<
+  sleep(1.seconds)
+end
+
+# describe '#countdown_with_sleep' do
+#     it 'should take at least 5 seconds to execute' do
+#       runtime = Benchmark.measure { countdown_with_sleep(5) }
+#       expect(runtime.real.to_i >= 5).to be true
+#     end
