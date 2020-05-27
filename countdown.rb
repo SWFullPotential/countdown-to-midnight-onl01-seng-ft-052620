d4822b53a4d9aaa5e4de2799fb8ddbc3c7a499d2
Samantha Wabi <swfullpotential@gmail.com>
2020-05-27 20:30:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number >= 1
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+def countdown_with_sleep(sec)
+  while sec > 0 do
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+    sleep(1)
+  end
+end
+  return "HAPPY NEW YEAR!"
 end
